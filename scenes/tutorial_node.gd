e5a9346571eb16ef96a3e5d69e9337da1c2059e9
Logan <logankeenan55@gmail.com>
2025-08-25 13:13:07 -0400

    Added to tutorial
    Have to wait before doing tutorial nodes, and added a lot

--- scenes/tutorial_node.gd
+++ scenes/tutorial_node.gd
@@ -2,16 +2,15 @@ extends Node2D
 class_name TutorialNode
 
 var actionNeeded:String
-var offset = Vector2(15, 70)
+var canClose = false
+
+func _ready() -> void:
+	$AnimationPlayer.play("Open")
+	z_index = 30
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed(actionNeeded):
+	if canClose and Input.is_action_just_pressed(actionNeeded):
 		$AnimationPlayer.play("Close")
-	position = GlobalVars.player.position + offset
-func _ready() -> void:
-	if self != null:
-		$AnimationPlayer.play("Open")
-		z_index = 30
 
 func setText(text:String):
 	$ColorRect/Label.text = text
@@ -21,7 +20,8 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "Close":
 		queue_free()
 
-func constructor(text:String, newActionNeeded:String):
+func constructor(text:String, newActionNeeded:String, waitTime:float):
 	setText(text)
 	actionNeeded = newActionNeeded
-	
+	await get_tree().create_timer(waitTime).timeout
+	canClose = true
--- tutorial_node_spawner.gd
+++ tutorial_node_spawner.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+class_name TutorialNodeSpawner
 #System for spawning tutorial nodes during new game. Add to setArr() to add tutorial Nodes. "string", "input"
 
 var nodesArr = []
@@ -7,30 +7,50 @@ var nodeIndex = 0
 var tutorialNodePreload = preload("res://scenes/TutorialNode.tscn")
 var started = false
 
+#for not letting player continue tutorial without waiting for waitTime bewteen nodes
+var canContinue = true
+var waitTime = 3
+
 func _ready() -> void:
 	setArr()
 
 func _process(delta: float) -> void:
-	if !started:
+	if !started or !canContinue:
 		return
 	var input = nodesArr[nodeIndex][1]
 	if Input.is_action_just_pressed(input):
 		nodeIndex += 1
+		
+		
 		if nodeIndex >= nodesArr.size():
 			queue_free()
 			return
-		createTutorialNode(nodesArr[nodeIndex][0],nodesArr[nodeIndex][1])
-		
+		createTutorialNode(nodesArr[nodeIndex][0],nodesArr[nodeIndex][1], waitTime)
+		canContinue = false
+		await get_tree().create_timer(waitTime).timeout
+		canContinue = true
 
 func setArr() -> void:
 	nodesArr.append(["Use WASD to move", "w"])
 	nodesArr.append(["Use E and F to switch between placing and farming mode", "f"])
+	nodesArr.append(["Use number keys to switch between hotbar slots", "3"])
+	nodesArr.append(["Equip dirt and press left click to place", "click"])
+	nodesArr.append(["Equip water and place next to dirt to fertalize it", "click"])
+	nodesArr.append(["Go to farming mode with f and equip seeds with 3", "3"])
+	nodesArr.append(["Click on a farm tile with seeds equip to plant", "click"])
+	nodesArr.append(["While seeds are equipped, you can hit 3 again to switch seeds", "3"])
+	nodesArr.append(["Switch to hoe to harvest crops", "1"])
+	nodesArr.append(["Use tab to open shop menu. You can buy bamboo for wheat", "tab"])
+	#nodesArr.append(["Use ", ""]) <= STARTER
 
 func start() -> void:
-	print("yay")
-	createTutorialNode(nodesArr[nodeIndex][0],nodesArr[nodeIndex][1])
+	createTutorialNode(nodesArr[nodeIndex][0],nodesArr[nodeIndex][1], waitTime)
+	canContinue = false
+	await get_tree().create_timer(waitTime).timeout
+	canContinue = true
 	started = true
-func createTutorialNode(text:String, input: String) -> void:
+	
+func createTutorialNode(text:String, input: String, waitTime: float) -> void:
 	var newTutorialNode: TutorialNode = tutorialNodePreload.instantiate()
-	add_child(newTutorialNode)
-	newTutorialNode.constructor(text, input)
+	GlobalVars.player.add_child(newTutorialNode)
+	newTutorialNode.constructor(text, input, waitTime)
