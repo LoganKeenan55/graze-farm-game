823d8b23665be8dd0513b992b746922790d0edce
Logan <logankeenan55@gmail.com>
2025-10-25 14:51:56 -0400

    Fade in to main menu
    Black fade in when starting game

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -7,6 +7,7 @@ func _ready() -> void:
 	SoundManager.player = self
 	await get_tree().create_timer(.3).timeout
 	$MainMenuCursor/Area2D.monitoring = true
+	$FadeIn.play("Fade")
 func _on_continue_button_mouse_entered() -> void:
 	SoundManager.play_ui_sound("res://sounds/bloop7.mp3")
 	
